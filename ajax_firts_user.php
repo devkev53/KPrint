f69a991689fc65df4038cca754d1aa367ebcdc3d
Kevin Alberto Palma Ralda <47469351+kapr53@users.noreply.github.com>
2020-10-23 22:49:27 -0600

    Add files via upload
    Se adjunto el archivo para crear el primer usuario..!

---
+++ ajax_firts_user.php
@@ -0,0 +1,37 @@
+<?php 
+	
+	$user = strtoupper($_POST['usuario']);
+	$email = strtoupper($_POST['correo']);
+	$pass1 = $_POST['contraseña1'];
+	$pass2 = $_POST['contraseña1'];
+	$tipo = $_POST['tipo'];
+
+	// Hacemos la coneccion a la DB
+	require 'database.php';
+
+	require 'prueba_correo.php';
+	require 'correo_bienvenida.php';
+		
+	if ($pass1==$pass2) {
+		// Hacemos un hash con el pasword encriptando el mismo
+		$passwordHash = password_hash($pass1, PASSWORD_DEFAULT);
+
+		// Creando el query o consulta de  usuarios
+		$qs_insert = "INSERT INTO usuario (codigo, usuario, email, clave, fecha_crea, fecha_act, tipo)
+			VALUES(null, '$user', '$email', '$passwordHash', CURRENT_DATE, CURRENT_DATE, $tipo)";
+		// Insertando el usuario con el query
+		$insert = mysqli_query($conn, $qs_insert);
+
+		if ($insert) {
+			echo json_encode(array('error' => false));
+		}
+
+		$cuerpo = bienvenida_usuario($user, $email);
+
+		$asunto = "Bienvenido a KodePrint";
+
+		if (enviar_email($email, $user, $asunto, $cuerpo)) {
+			echo json_encode(array('error' => false));
+		}
+	}
+?>
