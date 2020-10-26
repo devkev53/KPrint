3a73e27556fec76bde47aca04a62faf35b07385a
Kevin Alberto Palma Ralda <47469351+kapr53@users.noreply.github.com>
2020-10-26 11:10:09 -0600

    Update coneccion_cloudidanry.php

--- coneccion_cloudidanry.php
+++ coneccion_cloudidanry.php
@@ -1,17 +1,3 @@
-<?php  
-  require  'cloudinary/autoload.php' ;
-  require  'cloudinary/src/Helpers.php' ;
-
-  \Cloudinary::config(array( 
-    "cloud_name" => "hfub7pce6", 
-    "api_key" => "513517168346165", 
-    "api_secret" => "tuWuIUas7rFLtZXOxQPkSJl-PG0", 
-    "secure" => true
-  ));
-
-  \Cloudinary\Uploader::upload("/C:/Users/kpalma/Pictures/Kevin.png")
-  
+<?php 
   echo "Hola cloudinary";
-
-  
  ?>
