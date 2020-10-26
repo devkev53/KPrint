081003c59f87c0d2cd76426afa91c3381cfeb3dd
Kevin Alberto Palma Ralda <47469351+kapr53@users.noreply.github.com>
2020-10-25 23:56:50 -0600

    Update ajax_delete_dev.php

--- Internos/ajax_delete_dev.php
+++ Internos/ajax_delete_dev.php
@@ -22,22 +22,17 @@
 	// Validamos si nuestra consulta trajo algun resultado
 	if (mysqli_num_rows($user) > 0) {
 
-		echo "Encontro un Usuario";
-		echo gettype ( $codigo_user );
-
+		// Realizando la consulta
+		$dev_con = mysqli_query($conn, "DELETE FROM heroku_59b4c55ab4de36a.desarrollador WHERE codigo=$codigo");
+		
 		// Eliminamos el dato que se encontro
-		//$user_delete = mysqli_query($conn, "DELETE FROM heroku_59b4c55ab4de36a.usuario WHERE codigo=$codigo_user");
-		echo "DELETE FROM heroku_59b4c55ab4de36a.usuario WHERE codigo=$codigo_user";
+		$user_delete = mysqli_query($conn, "DELETE FROM heroku_59b4c55ab4de36a.usuario WHERE codigo=$codigo_user");
 	}
-
-	// Realizando la consulta
-	//$dev_con = mysqli_query($conn, "DELETE FROM heroku_59b4c55ab4de36a.desarrollador WHERE codigo=$codigo");
-	//if($dev_con){ echo "Se Elimino Correctamente el Desarrollador";}
 	
 	//Verificamos si el usuario que se elimino era el usuario en linea
-	/*if ($user==$inline) {
+	if ($user==$inline) {
 		header('location: ../logout.php');
 	}else{
 		header('location: ../Internos/desarrolladores.php');
-	}*/
+	}
 ?>
