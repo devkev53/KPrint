e38e77b913c95683250254e275431fc3b7057c51
Kevin Alberto Palma Ralda <47469351+kapr53@users.noreply.github.com>
2020-10-25 22:05:03 -0600

    Update devs.php

--- Externos/devs.php
+++ Externos/devs.php
@@ -36,7 +36,9 @@
 	$dev_x_pagina = 4;
 	// No de paginas a mostrar
 	$paginas = ceil($no_dev/$dev_x_pagina);
-	//echo $paginas;
+	if ($paginas<=0) {
+		$paginas += 1;
+	}
 
 	// Validando para los paginadores
 	if (!$_GET) {
