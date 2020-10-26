6d56ba063cd1b63b37d5028a6dad811d0def6b06
Kevin Alberto Palma Ralda <47469351+kapr53@users.noreply.github.com>
2020-10-25 23:07:59 -0600

    Update ajax_delete_dev.php

--- Internos/ajax_delete_dev.php
+++ Internos/ajax_delete_dev.php
@@ -30,12 +30,12 @@
 
 		// Eliminamos el dato que se encontro
 		$user_delete = mysqli_query($conn, "DELETE FROM heroku_59b4c55ab4de36a.usuario WHERE codigo=$codigo_user");
-		if($user){echo "Se elimino el usuario";}else{echo "No fue posible eliminar el usuario";
+		if($user){echo "Se elimino el usuario";}else{echo "No fue posible eliminar el usuario";}
 	}
 
 	// Realizando la consulta
 	$dev_con = mysqli_query($conn, "DELETE FROM heroku_59b4c55ab4de36a.desarrollador WHERE codigo=$codigo");		
-	if($dev_con){echo "Se elimino el desarrollador";}else{echo "Error en la consulta";
+	if($dev_con){echo "Se elimino el desarrollador";}else{echo "Error en la consulta";}
 
 	// Verificamos si el usuario que se elimino era el usuario en linea
 	/*if ($user==$inline) {
